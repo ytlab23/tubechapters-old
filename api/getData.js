92ff643e3caf3ca88a4af760213a702644c81c4c
daniel endaylalu <endaylaludaniel@gmail.com>
2024-05-14 14:22:25 +0300

    adde custom UserAgent

--- api/getData.js
+++ api/getData.js
@@ -12,13 +12,14 @@ export const get_data = async (url) => {
     const response = await axios.get(url, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0", // Firefox on Windows
       },
     });
     const data = await response.data;
+    console.log(data);
     return data;
   } catch (err) {
-    console.log(err);
+    console.log("error", err);
   }
 };
 
