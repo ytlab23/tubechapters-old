70a5cf041daadecc7426ae9e6cb631e5b9849eb6
daniel endaylalu <endaylaludaniel@gmail.com>
2024-06-20 21:21:08 +0300

    added logo and favicon

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../app/logo.png';
 
 const Nav = () => {
   return (
     <nav className="flex items-center justify-around py-4">
       <Link href="/" className="text-2xl font-bold text-[#dc2626]">
-        tubechapters
+        <Image src={logo} alt="tubechapters logo" className="max-w-[100px]" />
       </Link>
       <div className="flex gap-x-4 sm:gap-x-8 items-center">
         <Link
